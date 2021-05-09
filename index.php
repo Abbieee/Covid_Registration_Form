6a7ae6c44f5e4a314feb62f284b574ff4a66a4ee
Abbieee <46325196+Abbieee@users.noreply.github.com>
2021-05-09 18:46:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$servername = "localhost";
+$username = "root";
+$password = "tiger123";
+$dbname = "registration";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+	
+	//echo "Database Successfully Connected";
---
+++ read.php
@@ -0,0 +1,16 @@
+<?php
+include("index.php");
+
+$sql = "SELECT  * FROM user_detail";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["UserID"]. " - Name: " . $row["FirstName"]. " " . $row["LastName"]. " - Email: " . $row["Email"]. " " ." - PhotoIdProof: " . $row["PhotoIdProof"]. " " . " - PhotoIdNumber: " . $row["PhotoIdNumber"]. " " . " - Gender: " . $row["Gender"]. " " . " - Age: " . $row["Age"]. " " . " - MedicalIssue: " . $row["MedicalIssue"]. " " .   "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
