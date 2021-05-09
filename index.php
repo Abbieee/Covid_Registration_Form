751596691f6373a1e469fdc1260a33679f488bc2
Abbieee <46325196+Abbieee@users.noreply.github.com>
2021-05-09 19:27:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$servername = "localhost";
-$username = "root";
-$password = "tiger123";
-$dbname = "registration";
+$servername = "remotemysql.com";
+$username = "0hLnuC7qN4";
+$password = "I0dsja7vnp";
+$dbname = "0hLnuC7qN4";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
